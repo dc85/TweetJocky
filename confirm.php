3ec17322f6a3b3a94e09c99b6d41d98fb05c7704
Daniel Chen <daniel85.chen@gmail.com>
2010-04-15 21:39:29 -0400

    added confirm

--- confirm.php
+++ confirm.php
@@ -3,6 +3,7 @@ include 'EpiCurl.php';
 include 'EpiOAuth.php';
 include 'EpiTwitter.php';
 include 'secret.php';
+include 'mysqldb.php';
 
 $twitterObj = new EpiTwitter($consumer_key, $consumer_secret);
 
@@ -11,8 +12,33 @@ $token = $twitterObj->getAccessToken();
 $twitterObj->setToken($token->oauth_token, $token->oauth_token_secret);
 $twitterInfo= $twitterObj->get_accountVerify_credentials();
 $twitterInfo->response;
-echo "Your twitter username is {$twitterInfo->screen_name} and your profile picture is <img src=\"{$twitterInfo->profile_image_url}\">";
+echo "Your twitter username is {$twitterInfo->screen_name} and your profile picture is <img src=\"{$twitterInfo->profile_image_url}\"> twitter follower {$twitterInfo->followers_count}";
+
+echo "<br />POST<br />";
+var_dump($_POST);
+echo "<br />GET<br />";
+var_dump($_GET);
+echo "<br />TwitterInfo<br />";
+print_r(twitterInfo);
 $tok = file_put_contents('tok', $token->oauth_token);
 $sec = file_put_contents('sec', $token->oauth_token_secret);
-?>
+
+function checkAccount() {
+	if($db = new MySQLDB) {
+		$query = "SELECT * FROM tblAccounts WHERE a_T_id=".$twitterInfo->id;
+		$db->begin();
+		if($result = mysql_query($query)) {
+			if(mysql_num_rows($result) == 1) {
+				return true;
+			} else if(mysql_num_rows($result) <= 0) {
+				return false;
+			} else {
+				$db->logEvent("checkAccount","$twitterInfo->id","Duplicate entries in tblAccount");
+			}
+		} else {
+			$db->logEvent("checkAccount","admin","Query Error: ".mysql_error()ot just);
+		}
+	}
+}
+?> 
 
